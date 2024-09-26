cfc5b91b89d1db50ee9f1f06b4daf0c6d1aa04fc
Bandan Pradhan <bandan472000@gmail.com>
2024-09-27 00:55:08 +0530

    configure utills files

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponce{
+    constructor(statusCode, data, message = "success", succss) {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.succss = statusCode <400
+      
+     }
+}
