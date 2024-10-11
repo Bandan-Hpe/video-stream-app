e08a6f58e65c9afead7678c9fd92c79cc86a3cfb
Bandan Pradhan <bandan472000@gmail.com>
2024-10-11 20:38:52 +0530

    configure cloudinary and multer for file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localpath) => {
+  try {
+    if (!localpath) return null;
+    // upload the file on cloudinary
+    const res = await cloudinary.uploader.upload(localpath, {
+      resource_type: "auto",
+    });
+    // file has been successfully uploaded
+    console.log("File uploaded successfully", res.url);
+    return res;
+  } catch (error) {
+    fs.unlinkSync(localpath);
+  }
+};
